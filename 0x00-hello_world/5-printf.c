8ff96f75e6f70a59b046dc912f656d3798ea2616
Emebetyw <emuyoha21@gmail.com>
2022-06-02 10:42:53 -0700

    print text using printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Reaturn: Always 0 (Success)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a peice of art,\n"); 
+        return(0);
+
+} 
