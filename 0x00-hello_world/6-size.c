fdb7d560e7c8aeda5dd2f3e90e57bb1b4b91fb08
Emebetyw <emuyoha21@gmail.com>
2022-06-02 11:26:12 -0700

    check all data zsize

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,12 +17,12 @@ int main(void)
 
 	float f;
 
-printf("Size fo a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+        printf("Size fo a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+        printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+        printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+        printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 
-return (0)
+        return (0);
 }	
 
