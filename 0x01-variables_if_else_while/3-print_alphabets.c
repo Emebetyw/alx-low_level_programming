f776032ec79a82cdcbbd569582740e763b7e703d
Emebetyw <Emuyoha21@gmail.com>
2022-06-04 21:37:26 +0300

    print alphabet in upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main -prints both small alphabets and capital alphabets
+ * Return : always return 0 (success).
+ */
+int main(void)
+{
+	char c;
+	for(c='a';c<='z';c++)
+		putchar (c);
+	 
+	for (c='A';c<='Z';c++)
+		puchar(c);
+
+	putchar('/n');
+	 
+
+	return (0);
+
+}	
