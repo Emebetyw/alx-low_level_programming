b8231e2af3cd6ec9c768d9c06f5c7477e3d7bb2f
Emebetyw <Emuyoha21@gmail.com>
2022-06-05 18:42:18 +0300

    print 1upto 9 with space and comma sparated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entru\y point.
+ *
+ * Return: Always returns zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '1'; n <= '8'; n++)
+		putchar('0');
+			putchar(", ",n);
+				putchar('9');
+	putchar('\n');
+}	
