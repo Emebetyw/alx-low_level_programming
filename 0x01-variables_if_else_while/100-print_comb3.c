67b3f515892b583ba75c188c97f3d29723c813e7
Emebetyw <Emuyoha21@gmail.com>
2022-06-07 10:19:28 +0300

    comination of two digit in small number format

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,12 +10,18 @@ int main(void)
 	int i, n;
 	
 	for (i ='0'; i <='9'; i++)
-		for (n ='0'; n <= '9'; n++)
+	{
+		for (n ='0'; n <= '9'; n++){
 			putchar(i);
 			putchar(n);
+
 			if ((i != '9') || (i == '9' && n != '9'))
+			{
 				putchar(',');
 				putchar(' ');
+			}
+		}
+	}
 	putchar('\n');
 	return (0);	
 }
