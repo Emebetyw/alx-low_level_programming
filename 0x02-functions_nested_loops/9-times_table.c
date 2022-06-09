e126579fbf6b8c773f32aa3c6e0bc1f18f874bf4
Emebetyw <Emuyoha21@gmail.com>
2022-06-09 17:10:20 +0300

    added nine times table starting from zero

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 /**
  * times_tabel - prints the 9 times table, starting with 0.
  *
- * Return: returns nothing;
+ * Return: returns nothing
  */
 
 void times_table(void)
@@ -15,18 +15,18 @@ void times_table(void)
 	{
 		_putchar('0');
 
-			for (mult = 1; mult <= 9; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				result = digit * mult;
-				if (result <= 9)
-					_putchar(' ');
-				else
-					_putchar((result / 10) + '0');
+	for (mult = 1; mult <= 9; mult++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		result = digit * mult;
+		if (result <= 9)
+		_putchar(' ');
+	else
+		_putchar((result / 10) + '0');
 
-				_putchar((result % 10) + '0');
-			}
-			_putchar('\n');
-		}
+		_putchar((result % 10) + '0');
+	}
+		_putchar('\n');
+	}
 }
