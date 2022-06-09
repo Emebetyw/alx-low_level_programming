fd27fc34b776cb9bced5cf3e5b17b0d1cf501ba6
Emebetyw <Emuyoha21@gmail.com>
2022-06-09 12:10:56 +0300

    print lower case alphabets in

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,18 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int count = 1;
+	int count;
+
+	count = 1;
+
 
 	while (count <= 10)
-	for (c = 'a'; c <= 'z'; c++)
-	_putchar(c);
-	_putchar('\n');
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 	count++;
+	_putchar('\n');
+	}
 }
