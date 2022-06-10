6f0baf6816b4e3d38afe514ee54f9f97cb9f2b84
Emebetyw <Emuyoha21@gmail.com>
2022-06-10 10:28:11 +0300

    a function checks upper case character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - function that checks upper case character
+ * @c: chracter to be checked
+ *
+ * Return: 1 if the character is upper, 0 if not
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (C <= 'Z'))
+		return (1);
+	else 
+		return (0)
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+/**
+ * File:main.h
+ * Author:Emebetyw
+ * Desc:header file containing functions declaration
+ */
+int _isupper(int c);
+
+
