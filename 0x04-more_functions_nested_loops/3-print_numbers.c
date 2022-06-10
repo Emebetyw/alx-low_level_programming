622e799739b0e7765fa141c4f9a12b8627292631
Emebetyw <Emuyoha21@gmail.com>
2022-06-10 18:29:15 +0300

    function prints number 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Functions that prints 0 up to 9
+ * @n:  number 0 to 9
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	for (n = 48; n <= 57; n++)
+
+		_putchar(n);
+}
