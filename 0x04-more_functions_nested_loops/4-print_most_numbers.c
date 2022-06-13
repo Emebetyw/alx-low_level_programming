d301180cd7713d771e27cee49d4d0b9e513267f6
Emebetyw <Emuyoha21@gmail.com>
2022-06-13 10:44:36 +0300

    print 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 48; n <= 57; n++)
 	{
-		if ((n == '2') || (n == '4'))
+		if ((n == 50) || (n == 52))
 			continue;
 		else
-			_putchar(n + '0');
+			_putchar(n);
 	}
 	_putchar('\n');
 }
