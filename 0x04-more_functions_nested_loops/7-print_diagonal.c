6f80640b19c64ac89b890e9263504edd9a11c7c0
Emebetyw <Emuyoha21@gmail.com>
2022-06-13 17:47:18 +0300

    print n times diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,13 +7,16 @@
 void print_diagonal(int n)
 {
 	int i = 0;
+
 	while (i < n)
 	{
-		if (n < 0){
+		if (n < 0)
+		{
 			_putchar('\n');
 		}
-		_puchar('\\');
-		i++
+		_putchar('\\');
+		_putchar('\n');
+		i++;
 	}
 	_putchar('\n');
 }
