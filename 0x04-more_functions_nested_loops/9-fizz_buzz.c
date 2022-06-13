e9bae3b884be967a967e95657637ce125810dd36
Emebetyw <Emuyoha21@gmail.com>
2022-06-13 21:15:19 +0300

    print fizz buzz for different comditions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,22 @@ int main(void)
 	{
 		if(num % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		printf("%d", num);
 		if (num != 100)
 			printf(" ");
-
 	}
+
+
 	printf("\n");
 	return (0);
 
